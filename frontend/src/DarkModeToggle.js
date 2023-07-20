f82f69ac170a293e4ea054fc7e1c52e0c70813b6
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-07-20 23:09:35 +0900

    다크모드 설정

---
+++ frontend/src/DarkModeToggle.js
@@ -0,0 +1,33 @@
+class DarkModeToggle {
+  isDarkMode = null;
+  $DarkModeToggle = null;
+  constructor({ $target, onSearch }) {
+    const $DarkModeToggle = document.createElement("input");
+    this.$DarkModeToggle = $DarkModeToggle;
+    $DarkModeToggle.type = "checkbox";
+    this.$DarkModeToggle = $DarkModeToggle;
+
+    $DarkModeToggle.className = "dark-mode";
+    $target.appendChild($DarkModeToggle);
+
+    $DarkModeToggle.addEventListener("change", (e) => {
+      console.log(e.target.checked);
+      this.setColorMode(e.target.checked);
+    });
+    this.initColorMode();
+  }
+  render() {}
+
+  initColorMode() {
+    this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    this.$DarkModeToggle.checked = this.isDarkMode;
+    this.setColorMode(this.isDarkMode);
+  }
+
+  setColorMode(isDarkMode) {
+    document.documentElement.setAttribute(
+      "color-mode",
+      isDarkMode ? "dark" : "light"
+    );
+  }
+}
