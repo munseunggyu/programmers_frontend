950a6accfc214d1b589e5d3fecf42ee3d1b2af36
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-07-25 23:14:53 +0900

    에러처리 추가

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,31 @@
 const API_ENDPOINT = "http://localhost:4001";
 
+const REQUSET_ERROR = {
+  400: { msg: "400 요청실패" },
+  500: { msg: "500 요청실패" },
+};
+
 const api = {
   fetchCats: async (keyword, page) => {
-    const res = await fetch(
-      `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}&limit=15`
-    );
-    const data = await res.json();
-    return data;
+    try {
+      const res = await fetch(
+        `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}&limit=15`
+      );
+      console.log(res);
+      console.log(res.status);
+      if (res.status !== 200) {
+        throw REQUSET_ERROR[res.status];
+      } else {
+        const data = await res.json();
+        return data;
+      }
+    } catch (err) {
+      console.log(err);
+      alert(err.msg);
+      return {
+        data: null,
+      };
+    }
   },
   randomFetchCat: async () => {
     const res = await fetch(`${API_ENDPOINT}/api/cats/random50`);
